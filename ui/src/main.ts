4817992852ef48a8ae0488548b8ef220134e2dd5
Thomas Duboucher <thomas@duboucher.eu>
2023-01-07 22:49:57 +0100

    Prepare two screens to test router

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -14,11 +14,11 @@ async function greet() {
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document
-    .querySelector("#greet-button")
-    ?.addEventListener("click", () => greet());
+  window.addEventListener("hashchange", () => {
+    const hash = window.location.hash;
+
+    console.log(hash);
+  });
 });
 
 declare global {
