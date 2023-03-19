ce09f99a67b2c509c2fba527729b9133b5713b1f
Thomas Duboucher <thomas@duboucher.eu>
2023-03-19 23:13:08 +0100

    Start working on listing devices

--- oskman-schemas/src/schemas.rs
+++ oskman-schemas/src/schemas.rs
@@ -1,16 +1,22 @@
 #[derive(serde::Serialize, serde::Deserialize, schemars::JsonSchema)]
 pub struct Schemas {
-  pub fido_reset_command: FidoResetCommand,
-  pub fido_reset_response: FidoResetResponse,
+    pub fido_device_list: FidoDeviceList,
+    pub fido_reset_command: FidoResetCommand,
+    pub fido_reset_response: FidoResetResponse,
+}
+
+#[derive(serde::Serialize, serde::Deserialize, schemars::JsonSchema)]
+pub struct FidoDeviceList {
+    pub dev: String,
 }
 
 #[derive(serde::Serialize, serde::Deserialize, schemars::JsonSchema)]
 pub struct FidoResetCommand {
-  pub dev: String,
+    pub dev: String,
 }
 
 #[derive(serde::Serialize, serde::Deserialize, schemars::JsonSchema)]
 pub struct FidoResetResponse {
-  pub ret: i32,
-  pub message: String,
+    pub ret: i32,
+    pub message: String,
 }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 )]
 
 use log::debug;
-use oskman_schemas::schemas::{FidoResetCommand, FidoResetResponse};
+use oskman_schemas::schemas::*;
 
 fn from_ptr_to_string(ptr: *const i8) -> String {
     let raw_message = unsafe { core::ffi::CStr::from_ptr(ptr) };
@@ -28,6 +28,39 @@ fn fido_init(flags: i32) {
 }
 
 #[tauri::command]
+fn fido_list_devices() -> FidoDeviceList {
+    debug!("fido_list_devices");
+
+    let device_list: FidoDeviceList;
+
+    device_list = unsafe {
+        let mut dev_list = libfido2_sys::fido_dev_info_new(64);
+
+        let mut n: usize = 0;
+
+        let err = libfido2_sys::fido_dev_info_manifest(dev_list, 64, &mut n);
+
+        if err != libfido2_sys::FIDO_OK {}
+
+        let mut ret: FidoDeviceList = FidoDeviceList { dev: "".to_string() };
+
+        for dev_id in 0..n {
+            let dev = libfido2_sys::fido_dev_info_ptr(dev_list, dev_id);
+
+            ret = FidoDeviceList {
+                dev: from_ptr_to_string(libfido2_sys::fido_dev_info_path(dev)),
+            };
+        }
+
+        libfido2_sys::fido_dev_info_free(&mut dev_list, n);
+
+        ret
+    };
+
+    device_list
+}
+
+#[tauri::command]
 async fn fido_reset(parameters: FidoResetCommand) -> FidoResetResponse {
     debug!("fido_reset");
 
@@ -36,9 +69,11 @@ async fn fido_reset(parameters: FidoResetCommand) -> FidoResetResponse {
 
         libfido2_sys::fido_dev_open(dev, parameters.dev.as_bytes().as_ptr() as *const i8);
 
+        let ret = libfido2_sys::fido_dev_reset(dev);
+
         libfido2_sys::fido_dev_close(dev);
 
-        libfido2_sys::fido_dev_reset(dev)
+        ret
     };
 
     let message: String = from_ptr_to_string(unsafe { libfido2_sys::fido_strerr(ret) });
@@ -61,7 +96,11 @@ fn main() {
             }
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![fido_init, fido_reset])
+        .invoke_handler(tauri::generate_handler![
+            fido_init,
+            fido_list_devices,
+            fido_reset
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
